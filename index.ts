8c782e6a4ccb9944ec40ea26a4d67aba69dd4836
Scott Grodberg <scott@trwebco.com>
2025-01-13 05:58:07 -0500

    Extract method and docs.

--- index.ts
+++ index.ts
@@ -52,42 +52,18 @@ function processCommand(interaction: any) {
 }
 
 function question(interaction: any) {
-    // don't reply to self
+
+    // Don't reply to self
     if (interaction.author == client.user || interaction.author?.bot) {
         return
     }
+
     if (data.currentQuestion === -1) {
-        // Randomly choose a question
-        data.currentQuestion = Math.floor(Math.random() * data.sheet.length);
-
-        // reset the answers, get ready to assign
-        data.currentAnswers = [-1, -1, -1, -1];
-
-        // Randomly put the correct answer in one of the four slots        
-        data.currentAnswers[Math.floor(Math.random() * 4)] = data.currentQuestion;
-
-        // Start building a set to hold the answers, add the current question as one of the possible chocies
-        const answerSet = new Set<number>();
-        answerSet.add(data.currentQuestion);
-
-        // Randomly choose 3 more (incorrect) answers
-        while (answerSet.size < 4) {
-            answerSet.add(Math.floor(Math.random() * data.sheet.length)); // +1 to not select the header row
-        }
-
-        // Remove the answer that was blocking a random choice
-        answerSet.delete(data.currentQuestion);
-
-        // Iterate the answers, if still needs to be filled then fill it, otherwise skip over        
-        const answerArray = Array.from(answerSet);
-        for (let i = 0; i < 4; i++) {
-            if (data.currentAnswers[i] !== -1) {
-                continue;
-            }
-            data.currentAnswers[i] = answerArray.shift()!;
-        }
+        // No question is currently selected, select one now
+        selectQuestionAndAnswers();
     }
 
+    // Output the question
     let output = "";
     for (let i = 0; i < 4; i++) {
         if (data.question.has(i)) {
@@ -96,6 +72,7 @@ function question(interaction: any) {
     }
     output += "\n";
 
+    // Output the answers
     for (let i = 0; i < 4; i++) {
         for (let j = 0; j < 4; j++) {
             if (data.answer.has(j)) {
@@ -103,7 +80,44 @@ function question(interaction: any) {
             }
         }
     }
+
+    // Send the output
     interaction.reply(`${output}`);
+
+}
+
+
+function selectQuestionAndAnswers() {
+    // Randomly choose a question
+    data.currentQuestion = Math.floor(Math.random() * data.sheet.length);
+
+    // reset the answers, get ready to assign
+    data.currentAnswers = [-1, -1, -1, -1];
+
+    // Randomly put the correct answer in one of the four slots        
+    data.currentAnswers[Math.floor(Math.random() * 4)] = data.currentQuestion;
+
+    // Start building a set to hold the answers, add the current question as one of the possible chocies
+    const answerSet = new Set<number>();
+    answerSet.add(data.currentQuestion);
+
+    // Randomly choose 3 more (incorrect) answers
+    while (answerSet.size < 4) {
+        answerSet.add(Math.floor(Math.random() * data.sheet.length)); // +1 to not select the header row
+    }
+
+    // Remove the answer that was blocking a random choice
+    answerSet.delete(data.currentQuestion);
+
+    // Iterate the answers, if still needs to be filled then fill it, otherwise skip over        
+    const answerArray = Array.from(answerSet);
+    for (let i = 0; i < 4; i++) {
+        if (data.currentAnswers[i] !== -1) {
+            continue;
+        }
+        data.currentAnswers[i] = answerArray.shift()!;
+    }
+
 }
 
 function answer(interaction: any) {
