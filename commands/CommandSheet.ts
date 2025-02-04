2f167ce5b437436c143f8bf75032a3270e93ceab
Scott Grodberg <scott@trwebco.com>
2025-02-04 16:41:55 +0700

    Since the sheet has just been changed, reset state for all users.

--- commands/CommandSheet.ts
+++ commands/CommandSheet.ts
@@ -22,12 +22,24 @@ export class CommandSheet implements ICommand {
         return command;
     }
 
-    processCommand(interaction: ChatInputCommandInteraction): void {
+    async processCommand(interaction: ChatInputCommandInteraction): Promise<void> {
         const user = this.data.users.get(interaction.user.id)!;
         const url = interaction.options.getString("url")!;
         // TODO: Validate url
         user.server.sheetUrl = url;
-        this.commandLoadData.processCommand(interaction);
+        await this.commandLoadData.processCommand(interaction);
+
+        // All users in a server share the sheet.
+        // Since the sheet has just been changed,
+        //   all users state must be reset.
+        // Sorry to other users.
+        this.data.users.forEach(_user => {
+            if (_user.server !== user.server) {
+                return;
+            }
+            this.data.rollbackQA(_user);
+            this.data.setCategoriesToAll(_user);
+        });
 
     }
 }
